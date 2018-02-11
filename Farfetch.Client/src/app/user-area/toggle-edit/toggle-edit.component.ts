54997bc094b9513ff7a3481877d1138dc2775bc6
Luís Parada Pereira <luisparadapereira@gmail.com>
2018-02-11 11:50:18 +0000

    Added select all and select none buttons to client

--- Farfetch.Client/src/app/user-area/toggle-edit/toggle-edit.component.ts
+++ Farfetch.Client/src/app/user-area/toggle-edit/toggle-edit.component.ts
@@ -11,17 +11,19 @@ import { FarfetchClasses } from '../../api/modules/farfetch/farfetch.models.';
 })
 export class ToggleEditComponent implements OnInit {
 
-private insert = false;
+  private insert = false;
   private URL = '/Toggler';
-  private myToggle : FarfetchModels.ToggleDto;
+  private myToggle: FarfetchModels.ToggleDto;
   loading = false;
-  private serviceList : Array<FarfetchModels.ServiceDto>
-  private serviceMap : Map<string, FarfetchModels.ServiceDto>
-  private selectedServicesMap : Map<string, FarfetchModels.ServiceDto>
+  private serviceList: Array<FarfetchModels.ServiceDto>
+  private serviceMap: Map<string, FarfetchModels.ServiceDto>
+  private selectedServicesMap: Map<string, FarfetchModels.ServiceDto>
+  allSelected = false;
+  nonSelected = false;
 
   constructor(private http: HttpService, private route: ActivatedRoute,
     private router: Router) {
-      this.loading = true;
+    this.loading = true;
   }
 
   ngOnInit() {
@@ -34,31 +36,51 @@ private insert = false;
     return this.selectedServicesMap.has(serviceId);
   }
 
-  toggleService(serviceId:string) {
+  toggleService(serviceId: string) {
 
-    if (this.selectedServicesMap === undefined) { 
+    if (this.selectedServicesMap === undefined) {
       this.selectedServicesMap = new Map<string, FarfetchModels.ServiceDto>();
-      this.addServiceToToggle(serviceId); 
-      return; 
+      this.addServiceToToggle(serviceId);
+      return;
     }
-    if (this.selectedServicesMap.has(serviceId)) { 
-      this.removeServiceFromToggle(serviceId); 
+    if (this.selectedServicesMap.has(serviceId)) {
+      this.removeServiceFromToggle(serviceId);
       return;
     }
     this.addServiceToToggle(serviceId);
   }
 
-  updateServicesConnectedToToggle(){
+  updateServicesConnectedToToggle() {
     this.selectedServicesMap = new Map<string, FarfetchModels.ServiceDto>(
-      this.myToggle.serviceList.map(x => [x.id, x] as [string, FarfetchModels.ServiceDto] ));
+      this.myToggle.serviceList.map(x => [x.id, x] as [string, FarfetchModels.ServiceDto]));
   }
 
   addServiceToToggle(serviceId) {
     this.selectedServicesMap.set(serviceId, this.serviceMap.get(serviceId));
+    this.nonSelected = false;
+    if ( this.selectedServicesMap.size === this.serviceMap.size) {
+      this.allSelected = true;
+    }
   }
 
   removeServiceFromToggle(serviceId) {
     this.selectedServicesMap.delete(serviceId);
+    this.allSelected = false;
+    if ( this.selectedServicesMap.size === 0) {
+      this.nonSelected = true;
+    }
+  }
+
+  addAllServices() {
+    this.selectedServicesMap = this.serviceMap;
+    this.allSelected = true;
+    this.nonSelected = false;
+  }
+
+  removeAllServices() {
+    this.selectedServicesMap = new Map<string, FarfetchModels.ServiceDto>();
+    this.allSelected = false;
+    this.nonSelected = true;
   }
 
 
@@ -81,7 +103,7 @@ private insert = false;
       this.insert = true;
       this.myToggle = new FarfetchClasses.InsertToggle();
       this.loading = false;
-      
+
       return;
     }
     this.insert = false;
@@ -92,38 +114,37 @@ private insert = false;
       );
   }
 
-  private getServiceList()
-  {
+  private getServiceList() {
     this.http.get<FarfetchModels.TogglerMessage<Array<FarfetchModels.ServiceDto>>>('/Service')
-    .subscribe(
-      data => {
-        this.serviceList = data.result;
-        this.serviceMap = new Map<string, FarfetchModels.ServiceDto>(
-          this.serviceList.map(x => [x.id, x] as [string, FarfetchModels.ServiceDto] ));
-      },
-      error => this.error(error)
-    );
+      .subscribe(
+        data => {
+          this.serviceList = data.result;
+          this.serviceMap = new Map<string, FarfetchModels.ServiceDto>(
+            this.serviceList.map(x => [x.id, x] as [string, FarfetchModels.ServiceDto]));
+        },
+        error => this.error(error)
+      );
   }
 
   private save(toggle: FarfetchModels.ToggleDto) {
-if(this.selectedServicesMap === undefined)  { this.myToggle.serviceList = new Array<FarfetchModels.ServiceDto>(); }
-else {
-    this.myToggle.serviceList = Array.from( this.selectedServicesMap.values());
-  }
+    if (this.selectedServicesMap === undefined) { this.myToggle.serviceList = new Array<FarfetchModels.ServiceDto>(); }
+    else {
+      this.myToggle.serviceList = Array.from(this.selectedServicesMap.values());
+    }
 
     if (this.insert) {
       this.http.post<FarfetchModels.ToggleDto>(this.URL, toggle)
-      .subscribe(
-        data => this.successfulInput(data),
-        error => this.error(error)
-      );
+        .subscribe(
+          data => this.successfulInput(data),
+          error => this.error(error)
+        );
     } else {
-      
+
       this.http.put<FarfetchModels.ToggleDto>(this.URL, toggle)
-      .subscribe(
-        data => this.successfulInput(data),
-        error => this.error(error)
-      );
+        .subscribe(
+          data => this.successfulInput(data),
+          error => this.error(error)
+        );
     }
   }
 
